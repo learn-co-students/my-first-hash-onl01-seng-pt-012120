48699f0057efedf370cee6fb02740c67f63ea266
Roberto Livi <robertomlivi@outlook.com>
2020-03-16 02:26:19 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,14 +22,14 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
+  local_manifest = {
     "whale bone corsets" => 5,
     "porcelain vases" => 2,
     "oil paintings" => 3
   }
-  shipping_manifest["muskets"] = 2
-  shipping_manifest["gun powder"] = 4
+  local_manifest["muskets"] = 2
+  local_manifest["gun powder"] = 4
   
-  shipping_manifest
+  local_manifest
 
 end
